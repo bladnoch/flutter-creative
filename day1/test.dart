a6f607028b6d4ae32aa23c2dbaac5e6b6f5267bc
DK <dongwook5478@gmail.com>
2023-06-27 18:18:58 +0900

    update

--- day1/test.dart
+++ day1/test.dart
@@ -1,4 +1,43 @@
+import 'package:collection/collection.dart';
+
 void main(){
-  var as="we";
-  as="hello";
+  String name="nasd";
+  bool td=true;
+  double db=23.1244;
+  int asdf=332;
+
+  if(name.isNotEmpty){
+    print("name has someting");
+  }
+
+  var numbers= [
+    2,
+    3,
+    4,
+    3,
+    if(name.isNotEmpty) 44
+  ];
+
+  print(numbers.length);
+  print(numbers.isNotEmpty);
+  if(numbers.isNotEmpty){
+    numbers.add(0);
+  }
+
+  print("total price ${asdf}");
+  print("total price $asdf");
+
+  print("max : "+numbers.max.toString());
+  print("min : "+numbers.min.toString());
+
+  Map<String, int > dir={
+    "1":1,
+    "2":2,
+    "4":7,
+    "3":3,
+  };
+
+  print(dir.keys.toString()+"\n");
+  print(dir.values);
+  print(dir["4"]);
 }
