f661e9a1673a4514273b67eb4b9f82862c3e26a3
DK <dongwook5478@gmail.com>
2023-06-28 12:41:03 +0900

    update day 3

--- day1/test.dart
+++ day1/test.dart
@@ -1,5 +1,12 @@
 import 'package:collection/collection.dart';
 
+Map<String, int > dir={
+  "1":1,
+  "2":2,
+  "4":7,
+  "3":3,
+};
+
 void main(){
   String name="nasd";
   bool td=true;
@@ -30,14 +37,27 @@ void main(){
   print("max : "+numbers.max.toString());
   print("min : "+numbers.min.toString());
 
-  Map<String, int > dir={
-    "1":1,
-    "2":2,
-    "4":7,
-    "3":3,
-  };
+
 
   print(dir.keys.toString()+"\n");
   print(dir.values);
   print(dir["4"]);
+  print("next print is from pritnDir() method : ${printDir()}");
+  printDir();
+
 }
+
+String printDir({
+  int dir=12,
+}){
+  return dir.toString();
+}
+
+///named function은 parameter에 중괄호를 쳐준다 그럴경우 순서에 상관없이 map처럼 파라미터 이름과 value를 추가한다.
+///생성자처럼 번수를 주지 않았을 경우에도 default로 값을 지정해줄 수 있다.
+///
+/// left ?? right
+/// if (left==!null) => left
+/// if (left==null) => right
+///
+///
