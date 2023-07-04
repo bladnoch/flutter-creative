53c08164c6ea748057c31cf3940870fbc3980634
DK <dongwook5478@gmail.com>
2023-07-04 11:48:35 +0900

    update comments

--- week2day2/lib/main.dart
+++ week2day2/lib/main.dart
@@ -12,9 +12,11 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.red,
         body:Column(
           children: [
-            SizedBox(height: 80,),
+            SizedBox(height: 60,),
+
+            ///photo image, +
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40),
+              padding: EdgeInsets.symmetric(horizontal: 20),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -34,45 +36,61 @@ class MyApp extends StatelessWidget {
                 ],
               ),
             ),
+            SizedBox(height: 40,),
 
-            SizedBox(height: 30,),
-            Row(
+            /// MONDAY 16
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
               children: [
                 Text("MONDAY 16",
                   style: TextStyle( color: Colors.white,
-                    fontSize: 20,
+                    fontSize: 18,
                     fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10,),
+
+            ///TODAY, *, 17 18 19 20 21...
+            Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Container(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+
+                      /// Text : TODAY
+                      Text("TODAY ",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 48,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+
+                      /// Icon : circle
+                      Icon(Icons.circle,
+                        color: Colors.pink,
+                        size: 15,),
+
+                      ///Text : 17 18 19...
+                      Text(" 17 18 19 20 21 22 23 24 25 26 27 28 29",
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.6),
+                          fontSize: 46,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-            Container(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    Text("TODAY ",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 50,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    Icon(Icons.circle,
-                    color: Colors.pink,
-                    size: 15,),
-
-                    Text(" 17  18  19  20",
-                      style: TextStyle(
-                        color: Colors.white.withOpacity(0.6),
-                        fontSize: 48,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
               ),
-            )
+            ),
+
           ],
         ),
       ),
