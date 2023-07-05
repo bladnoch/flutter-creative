128fed5c7ac673034c60ad50a82591ba69a0fc9d
DK <dongwook5478@gmail.com>
2023-07-05 16:48:38 +0900

    update ch4

--- week2day3/lib/main.dart
+++ week2day3/lib/main.dart
@@ -12,29 +12,72 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<int> num=[];
+  bool showTitle =true;
 
-  void onClicked(){
+  void toggle(){
     setState(() {
-      num.add(num.length);
+      showTitle=!showTitle;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.redAccent[200],
+          )
+        ),
+      ),
       home: Scaffold(
         backgroundColor: Colors.white70,
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("click count",style: (TextStyle(fontSize: 30)),),
-              for(var n in num) Text("$n"),
-              IconButton(iconSize: 40, onPressed: onClicked, icon: Icon(Icons.add_box_rounded),),
+              showTitle ? Title() : Text("nothing"),
+              IconButton(onPressed: toggle, icon: Icon(Icons.remove_red_eye),),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+
+class Title extends StatefulWidget {
+  const Title({
+    super.key,
+  });
+
+  @override
+  State<Title> createState() => _TitleState();
+}
+
+class _TitleState extends State<Title> {
+
+  @override
+  void initState(){
+    super.initState();
+    print("hello");
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+    print("dispose");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("build");
+    return Text("Title here",
+      style: (
+          TextStyle(
+              fontSize: 30
+          )
       ),
     );
   }
