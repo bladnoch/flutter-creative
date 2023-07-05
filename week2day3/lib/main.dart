ceea06772edef39f3b686140155f35c178c5ff83
DK <dongwook5478@gmail.com>
2023-07-05 12:51:27 +0900

    update 4.0

--- week2day3/lib/main.dart
+++ week2day3/lib/main.dart
@@ -4,11 +4,38 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter=0;
+
+  void onClicked(){
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white70,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("click count",style: (TextStyle(fontSize: 30)),),
+              Text("${counter}",style: (TextStyle(fontSize: 30)),),
+              IconButton(iconSize: 40, onPressed: onClicked, icon: Icon(Icons.add_box_rounded),),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
