143a2234b76d63a86292d3de420bb3955d89beca
DK <dongwook5478@gmail.com>
2023-07-07 16:08:38 +0900

    update api variables

--- week2day5/lib/models/on_screen_movie_model.dart
+++ week2day5/lib/models/on_screen_movie_model.dart
@@ -1,6 +1,8 @@
 class OnScreenModel{
   final String image;
+  final String id;
 
   OnScreenModel.fromJson(Map<String, dynamic> json)
-      : image = "https://image.tmdb.org/t/p/w500${json['backdrop_path']}";
+      : image = "https://image.tmdb.org/t/p/w500${json['backdrop_path']}",
+        id="${json['id']}";
 }
--- week2day5/lib/models/popular_movie_model.dart
+++ week2day5/lib/models/popular_movie_model.dart
@@ -1,8 +1,10 @@
 class PopularModel{
   final String image;
   final String title;
+  final String id;
 
   PopularModel.fromJson(Map<String, dynamic> json)
       : image = "https://image.tmdb.org/t/p/w500${json['backdrop_path']}",
-        title="${json["original_title"]}";
+        title="${json["original_title"]}",
+        id="${json['id']}";
 }
