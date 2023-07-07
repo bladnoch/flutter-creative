9ee11660f332b9aea6a2305926b9414142ee5c2b
DK <dongwook5478@gmail.com>
2023-07-07 12:26:57 +0900

    add services files

---
+++ week2day5/lib/services/api_service_popular.dart
@@ -0,0 +1,17 @@
+class ApiService{
+  final String baseUrl ="https://movies-api.nomadcoders.workers.dev";
+
+  final String popular="popular";
+  final String nowPlaying="coming-soon";
+  final String movieDetail="movie?=id=";
+
+  void getPopularMovies(){
+
+  }
+
+  void getOnScreenMovies(){
+
+  }
+
+
+}
