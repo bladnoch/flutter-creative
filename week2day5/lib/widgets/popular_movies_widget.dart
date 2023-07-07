1f9879b2ea788f6eef30f025aa096caf393063e3
DK <dongwook5478@gmail.com>
2023-07-07 23:38:16 +0900

    add two more widget files

---
+++ week2day5/lib/widgets/popular_movies_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Movie extends StatelessWidget {
+  const Movie({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
