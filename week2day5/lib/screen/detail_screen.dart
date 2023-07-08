b4e23492f5d11d450ce04be1bc9fd4a6e76fba56
DK <dongwook5478@gmail.com>
2023-07-08 16:59:01 +0900

    update poster image

--- week2day5/lib/screen/detail_screen.dart
+++ week2day5/lib/screen/detail_screen.dart
@@ -20,6 +20,30 @@ class DetailScreen extends StatefulWidget {
 class _DetailScreenState extends State<DetailScreen> {
 
   late Future<MovieDetailModel> movie;
+  late String moviePoster;
+
+  void poseter(){
+    FutureBuilder(
+        future: movie,
+        builder: (context , snapshot){
+          if(snapshot.hasData){
+            snapshot.data!.poster_path;
+            return Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    snapshot.data!.poster_path,
+                  ),
+                ),
+              ),
+            );
+          }
+          return Container(
+          );
+          },
+    );
+  }
+
 
   @override
   void initState() {
@@ -29,7 +53,6 @@ class _DetailScreenState extends State<DetailScreen> {
 
   @override
   Widget build(BuildContext context) {
-
 
 
     return Scaffold(
@@ -49,143 +72,22 @@ class _DetailScreenState extends State<DetailScreen> {
           ),
         ),
       ),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-
-              ///Title
-              Flexible(
-                  flex: 7,
-                  child:Container(
-                    alignment: Alignment.bottomLeft,
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                    ),
-                    child: FutureBuilder(
-                      future: movie,
-                      builder: (context , snapshot){
-                        if(snapshot.hasData){
-                          return Text(snapshot.data!.title,
-                            style: TextStyle(
-                              color: Colors.white70,
-                              fontWeight: FontWeight.w800,
-                              fontSize: 30,
-                            ),
-                          );
-                        }
-                        return Text("please...");
-                      },
-                    ),
+      body: FutureBuilder(
+        future: movie,
+        builder: (context , snapshot){
+          if(snapshot.hasData){
+            snapshot.data!.poster_path;
+            return Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    snapshot.data!.poster_path,
                   ),
-              ),
-              SizedBox(height: 5,),
-
-              ///Stars
-              Flexible(
-                flex: 1,
-                child:Container(
-                  alignment: Alignment.topLeft,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                  ),
-                  child: FutureBuilder(
-                    future: movie,
-                    builder: (context , snapshot){
-                      if(snapshot.hasData){
-                        return Text("${snapshot.data!.vote_average} / 10",
-                          style: TextStyle(
-                            color: Colors.white70.withOpacity(0.6),
-                          ),
-                        );
-                      }
-                      return Text("please...");
-                    },
-                  ),
-
+                  fit: BoxFit.cover,
                 ),
               ),
-              SizedBox(height: 10,),
-
-
-              ///hrs/genre
-              Flexible(
-                flex: 1,
-                child:Container(
-                  alignment: Alignment.topLeft,
-                  decoration: BoxDecoration(
-                    color: Colors.pink,
-                  ),
-                  child:  FutureBuilder(
-                    future: movie,
-                    builder: (context , snapshot){
-                      if(snapshot.hasData){
-                        return Text("${snapshot.data!.runtime}min  |  ${snapshot.data!.genres}",
-                          style: TextStyle(
-                            color: Colors.white70.withOpacity(0.6),
-                          ),
-                        );
-                      }
-                      return Text("please...");
-                    },
-                  ),
-
-                ),
-              ),
-
-              ///Storyline
-              Flexible(
-                flex: 2,
-                child:Container(
-                  alignment: Alignment.bottomLeft,
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                  ),
-                  child:  Text("Storyline",style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 30,
-                    fontWeight: FontWeight.w600,
-                  ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 5,),
-
-              ///Storyline API
-              Flexible(
-                flex: 7,
-                child:Container(
-                  alignment: Alignment.topLeft,
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                  ),
-                  child: FutureBuilder(
-                    future: movie,
-                    builder: (context , snapshot){
-                      if(snapshot.hasData){
-                        return Text(snapshot.data!.overview,
-                          style: TextStyle(
-                            color: Colors.white70,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        );
-                      }
-                        return Text("please...");
-                    },
-                  )
-
-
-                ),
-              ),
-              Container(
-                clipBehavior: Clip.hardEdge,
-                height: 60,
-                width: 250,
+              child: Container(
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Colors.amber,
                     boxShadow:[
                       BoxShadow(
                           blurRadius:5,
@@ -194,18 +96,153 @@ class _DetailScreenState extends State<DetailScreen> {
                       )
                     ]
                 ),
-                  child: Center(
-                      child: Text(
-                          "Buy ticket",style: TextStyle(
-                        fontWeight: FontWeight.w900,
-                        fontSize: 16,
-                      ),),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+
+                      ///Title
+                      Flexible(
+                        flex: 7,
+                        child:Container(
+                          alignment: Alignment.bottomLeft,
+                          child: FutureBuilder(
+                            future: movie,
+                            builder: (context , snapshot){
+                              if(snapshot.hasData){
+                                return Text(snapshot.data!.title,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w800,
+                                    fontSize: 30,
+                                  ),
+                                );
+                              }
+                              return Text("please...");
+                            },
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 5,),
+
+                      ///Stars
+                      Flexible(
+                        flex: 1,
+                        child:Container(
+                          alignment: Alignment.topLeft,
+                          child: FutureBuilder(
+                            future: movie,
+                            builder: (context , snapshot){
+                              if(snapshot.hasData){
+                                return Text("${snapshot.data!.vote_average} / 10",
+                                  style: TextStyle(
+                                    color: Colors.white.withOpacity(0.6),
+                                  ),
+                                );
+                              }
+                              return Text("please...");
+                            },
+                          ),
+
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+
+                      ///hrs/genre
+                      Flexible(
+                        flex: 1,
+                        child:Container(
+                          alignment: Alignment.topLeft,
+                          child:  FutureBuilder(
+                            future: movie,
+                            builder: (context , snapshot){
+                              if(snapshot.hasData){
+                                return Text("${snapshot.data!.runtime}min  |  ${snapshot.data!.genres}",
+                                  style: TextStyle(
+                                    color: Colors.white.withOpacity(0.6),
+                                  ),
+                                );
+                              }
+                              return Text("please...");
+                            },
+                          ),
+
+                        ),
+                      ),
+
+                      ///Storyline
+                      Flexible(
+                        flex: 2,
+                        child:Container(
+                          alignment: Alignment.bottomLeft,
+                          child:  Text("Storyline",style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 5,),
+
+                      ///Storyline API
+                      Flexible(
+                        flex: 7,
+                        child:Container(
+                            alignment: Alignment.topLeft,
+                            child: FutureBuilder(
+                              future: movie,
+                              builder: (context , snapshot){
+                                if(snapshot.hasData){
+                                  return Text(snapshot.data!.overview,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  );
+                                }
+                                return Text("please...");
+                              },
+                            )
+
+
+                        ),
+                      ),
+                      Container(
+                        clipBehavior: Clip.hardEdge,
+                        height: 60,
+                        width: 250,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.amber,
+                            boxShadow:[
+                              BoxShadow(
+                                  blurRadius:5,
+                                  offset: Offset(1,1),
+                                  color: Colors.black.withOpacity(0.2)
+                              )
+                            ]
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Buy ticket",style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            fontSize: 16,
+                          ),),
+                        ),
+                      ),
+                      SizedBox(height: 60,),
+                    ],
                   ),
+                ),
               ),
-              SizedBox(height: 60,),
-            ],
-          ),
-        ),
+
+            );
+          }
+          return Container(
+          );
+        },
       ),
     );
   }
