94c5c87423c8b50a2b651eb8a4780c012f567b9c
DK <dongwook5478@gmail.com>
2023-07-12 02:24:50 +0900

    add singlescrollview on main screen

--- week2day5/lib/screen/home_screen.dart
+++ week2day5/lib/screen/home_screen.dart
@@ -21,127 +21,134 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body:
-      Column(
-        children: [
+      SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          height: 1000,
+          child: Column(
+            children: [
 
-          Flexible(
-            flex: 10,
-            child: Container(),),
+              ///empty space on top
+              Flexible(
+                flex: 15,
+                child: Container(),),
 
-          ///Text: popular movie
-          Flexible(
-            flex: 3,
-            child: Container(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20),
-                child: Text("Popular Movies",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w800,
-                    fontSize: 30,
+              ///Text: popular movie
+              Flexible(
+                flex: 3,
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Text("Popular Movies",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        fontSize: 30,
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ),
 
-          ///popular movies
-          Flexible(
-            flex: 16,
-            child: FutureBuilder(
-              future: popMovies,
-              builder: (context,snapshot){
-                if(snapshot.hasData){
-                  return Column(
-                    children: [
-                      SizedBox(height: 0,),
-                      Expanded(child: buildPopList(snapshot)),
-                    ],
-                  );
-                }
-                else{
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
-            ),
-          ),
-
-          ///Text : Now in Cinemas
-          Container(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 20),
-              child: Text("Now in Cinemas",
-                style: TextStyle(
-                  fontWeight: FontWeight.w800,
-                  fontSize: 30,
+              ///popular movies
+              Flexible(
+                flex: 16,
+                child: FutureBuilder(
+                  future: popMovies,
+                  builder: (context,snapshot){
+                    if(snapshot.hasData){
+                      return Column(
+                        children: [
+                          SizedBox(height: 0,),
+                          Expanded(child: buildPopList(snapshot)),
+                        ],
+                      );
+                    }
+                    else{
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
                 ),
               ),
-            ),
-          ),
 
-          ///on screen movies
-          Flexible(
-            flex: 13,
-            child: FutureBuilder(
-              future: onScreenMovies,
-              builder: (context,snapshot){
-                if(snapshot.hasData){
-                  return Column(
-                    children: [
-                      SizedBox(height: 0,),
-                      Expanded(child: buildOnScreenList(snapshot)),
-                    ],
-                  );
-                }
-                else{
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
-            ),
-          ),
-
-          ///text : Coming soon
-          Container(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Text("Coming soon",
-                style: TextStyle(
-                  fontWeight: FontWeight.w800,
-                  fontSize: 30,
+              ///Text : Now in Cinemas
+              Container(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 20),
+                  child: Text("Now in Cinemas",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      fontSize: 30,
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
 
-          ///coming movies
-          Flexible(
-            flex: 15,
-            child: FutureBuilder(
-              future: comingMovies,
-              builder: (context,snapshot){
-                if(snapshot.hasData){
-                  return Column(
-                    children: [
-                      SizedBox(height: 0,),
-                      Expanded(child: buildComingList(snapshot)),
-                    ],
-                  );
-                }
-                else{
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
-            ),
+              ///on screen movies
+              Flexible(
+                flex: 13,
+                child: FutureBuilder(
+                  future: onScreenMovies,
+                  builder: (context,snapshot){
+                    if(snapshot.hasData){
+                      return Column(
+                        children: [
+                          SizedBox(height: 0,),
+                          Expanded(child: buildOnScreenList(snapshot)),
+                        ],
+                      );
+                    }
+                    else{
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
+                ),
+              ),
+
+              ///text : Coming soon
+              Container(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Text("Coming soon",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+              ),
+
+              ///coming movies
+              Flexible(
+                flex: 15,
+                child: FutureBuilder(
+                  future: comingMovies,
+                  builder: (context,snapshot){
+                    if(snapshot.hasData){
+                      return Column(
+                        children: [
+                          SizedBox(height: 0,),
+                          Expanded(child: buildComingList(snapshot)),
+                        ],
+                      );
+                    }
+                    else{
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
